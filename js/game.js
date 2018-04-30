d6de181fae443372fc20bc3add3b3e42987c2818
Yury Staravoitau <MorfinAll@mail.ru>
2018-04-30 19:51:01 +0300

    fill-word-game-test-task/feature: Delete ulsess functions, renames function to actual state

--- js/game.js
+++ js/game.js
@@ -25,45 +25,40 @@ class GameInterface {
         return inputs.selectSize.value;
     }
 
-    static flip(id) {
-        this.game_match.tryToFlip(id);
+    static select(id) {
+        this.game_match.tryToSelect(id);
     }
 
-    static pressEnterOnFocusCard(id) {
+    static pressEnterOnFocusLetter(id) {
         if (event.code === 'Enter') {
-            GameInterface.flip(id);
+            GameInterface.select(id);
         }
     }
 
     static startGame() {
         const size = GameInterface.getSizeFromUserInput();
         const user_data = GameInterface.getUserDataFromInputs();
-        this.game_match = new Game(size, this.cardBack, user_data);
+        this.game_match = new Game(size, user_data);
         this.game_match.createGame();
     }
 
 }
 
 class Game {
-    constructor(size, cardBack = "img/cardBacks/cardBack-1.jpg", user_data) {
+    constructor(size, user_data) {
         this.user_data = user_data;
         this.size = size;
-        this.pictureCollection = [];
-        this.cardBack = cardBack;
         this.time = 0;
         this.setTimeOutTimer;
-        this.flippedImgId = "none";
-        this.enableFlip = true;
-        this.score_guessed_cards = 0;
+        this.wordscore_guessed_words = 0;
         this.name_localStorage = 'game_records-' + this.size;
+        this.selectedLetters = [];
     }
 
     createGame() {
         this.changeScene("main-menu_id", "game-scene_id");
-        this.setPictureCollectionDoubled("img/cards/", this.size / 2);
-        this.shuffleCollection(this.pictureCollection);
         this.buildGameField(this.size);
-        this.autofocusTheFirstCard();
+        this.autofocusTheFirstLetter();
         this.startTimer();
     }
 
@@ -72,116 +67,48 @@ class Game {
         document.getElementById(nextScene).hidden = false;
     }
 
-    //pictures must named as 1.jpg 2.jpg and etc
-    setPictureCollectionDoubled(dir, quantity) {
-        for (let i = 1; i <= quantity; i++) {
-            this.pictureCollection.push(dir + i + ".jpg");
-            this.pictureCollection.push(dir + i + ".jpg");
-        }
-    }
-
-    shuffleCollection(arrCollection) {
-        function compareRandom(a, b) { return Math.random() - 0.5; }
-        for (let i = 0; i < 100; i++) {
-            arrCollection.sort(compareRandom);
-        }
-    }
-
     buildGameField(size) {
         for (let i = 0; i < size; i++) {
-            this.createHTMLCard("card-" + i, i + 1);
+            this.createHTMLElementOfLetter("word-" + i, i + 1);
         }
-        this.applying_CSS_cardBack(this.cardBack);
-        this.applying_CSS_frontImgs(this.pictureCollection);
+        this.fillTheGameField("a");
     }
 
-    autofocusTheFirstCard() {
-        const theFirstCard = document.getElementById("card-0");
+    autofocusTheFirstLetter() {
+        const theFirstCard = document.getElementById("word-0");
         theFirstCard.focus();
     }
 
-    createHTMLCard(id, number_from_id_for_tab) {
-        const newFrontCard = document.createElement('figure');
-        newFrontCard.setAttribute("class", "front");
-        const newBackCard = document.createElement('figure');
-        newBackCard.setAttribute("class", "back");
+    createHTMLElementOfLetter(id, number_from_id_for_tab) {
         const newCard = document.createElement('div');
-        newCard.setAttribute("class", "card flipped");
+        newCard.setAttribute("class", "word");
         newCard.setAttribute("id", id);
-        newCard.setAttribute("OnClick", "GameInterface.flip('" + id + "')");
+        newCard.setAttribute("OnClick", "GameInterface.select('" + id + "')");
         newCard.setAttribute("tabindex", number_from_id_for_tab);
-        newCard.setAttribute("onkeydown", "GameInterface.pressEnterOnFocusCard('" + id + "')");
-        newCard.appendChild(newFrontCard);
-        newCard.appendChild(newBackCard);
+        newCard.setAttribute("onkeydown", "GameInterface.pressEnterOnFocusLetter('" + id + "')");
         const newSection = document.createElement('section');
-        newSection.setAttribute("class", "card-container");
+        newSection.setAttribute("class", "word-container");
         newSection.appendChild(newCard);
         document.getElementById("game-scene_id").appendChild(newSection);
     }
 
-    applying_CSS_cardBack(cardsBack) {
-        const backCards = document.getElementsByClassName("back");
-        for (let i = 0; i < backCards.length; i++) {
-            backCards[i].setAttribute("style", "background: url(" + this.cardBack + ") no-repeat; background-size: contain;");
+    fillTheGameField(letter) {
+        const fields = document.getElementsByClassName("word");
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].textContent = letter;
         }
     }
 
-    applying_CSS_frontImgs(collection) {
-        for (let i = 0; i < collection.length; i++) {
-            let frontCard = document.getElementById("card-" + i).firstElementChild;
-            frontCard.setAttribute("style", "background: url(" + collection[i] + ") no-repeat; background-size: contain;");
-        }
+    tryToSelect(currentCard_id) {
+        this.flipLogic(currentCard_id);
     }
 
-    tryToFlip(currentCard_id) {
-        if (this.enableFlip === true) {
-            this.flipLogic(currentCard_id);
-        }
-        else {
-            // add disable flip sound
-        }
-    }
-
-    // flipLogic = this game logic
     flipLogic(currentCard_id) {
-        // flipAnimationDuration can disable next card flipping to chosen delay
-        // i more like this game behaviour with flipAnimationDuration=0, but my last mentor more like with 2000ms
-        const flipAnimationDuration = 2000;
-        // if all cards not flipped
-        if (this.flippedImgId === "none") {
-            this.flippedImgId = currentCard_id;
-            this.flip(currentCard_id);
-        }
-        else {
-            if (currentCard_id !== this.flippedImgId) {
-                // if first flipped card === current flipped card
-                if (this.getBackgImgCard(currentCard_id) === this.getBackgImgCard(this.flippedImgId)) {
-                    this.flip(currentCard_id);
-                    this.deactivateOnClickElem(currentCard_id);
-                    this.deactivateOnClickElem(this.flippedImgId);
-                    this.fade_in_card_container(this.flippedImgId);
-                    this.fade_in_card_container(currentCard_id);
-                    this.flippedImgId = "none";
-                    this.score_guessed_cards++;
-                }
-                else {
-                    // if first flipped card != current flipped card
-                    this.flip(currentCard_id);
-                    this.flipDisablerOnTime(flipAnimationDuration * 2);
-                    this.flipBothCardsWithDelay(currentCard_id, this.flippedImgId, flipAnimationDuration);
-                    this.flippedImgId = "none";
-                }
-            }
-        }
-        if (this.chkWinCondition()) {
-            clearTimeout(this.setTimeOutTimer);
-            this.chkAndUpdateTop10LocalStorageRecords(this.name_localStorage, this.time, this.user_data);
-            this.showWinSceneWithDelay(2000);
-        };
+        this.select(currentCard_id);
     }
 
-    flip(id) {
-        this.htmlAttrToggle(id, "class", "card", "card flipped");
+    select(id) {
+        this.htmlAttrToggle(id, "class", "word", "word flipped");
     }
 
     deactivateOnClickElem(id) {
@@ -189,15 +116,15 @@ class Game {
     }
 
     fade_in_card_container(id) {
-        document.getElementById(id).parentElement.setAttribute("class", "card-container fade-in");
+        document.getElementById(id).parentElement.setAttribute("class", "word-container fade-in");
     }
 
     htmlAttrToggle(id, attr, toggleClassOn, toggleClassOff) {
-        const card = document.getElementById(id);
-        if (card.getAttribute(attr) === toggleClassOn) {
-            card.setAttribute(attr, toggleClassOff);
+        const word = document.getElementById(id);
+        if (word.getAttribute(attr) === toggleClassOn) {
+            word.setAttribute(attr, toggleClassOff);
         } else {
-            card.setAttribute(attr, toggleClassOn);
+            word.setAttribute(attr, toggleClassOn);
         }
     }
 
@@ -207,7 +134,7 @@ class Game {
     }
 
     chkWinCondition() {
-        return this.score_guessed_cards === this.size / 2;
+        return this.wordscore_guessed_words === this.size / 2;
     }
 
     showCongrat() {
@@ -240,8 +167,8 @@ class Game {
     flipBothCardsWithDelay(id1, id2, ms) {
         const that = this;
         function enabler() {
-            that.flip(id1);
-            that.flip(id2);
+            that.select(id1);
+            that.select(id2);
         }
         setTimeout(enabler, ms);
     }
